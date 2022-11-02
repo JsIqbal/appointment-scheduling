3aa1550ab2cab472c2eced0c13d3865af82a3fc5
JsIqbal <javascriptiqbal@gmail.com>
2022-11-03 04:07:54 +0600

    Initialize DB

--- src/modules/admin/admin.controller.js
+++ src/modules/admin/admin.controller.js
@@ -1,11 +1,26 @@
-const admin = (req, res) => {
-    console.log("Hello");
+const Admin = require('../admin/admin.model');
+
+const login = async (req, res) => {
     try {
-        res.send("Request Recieved");
-    }
-    catch(err) {
-        console.log(err);
+        const { email, password } = req.body;
+
+        const admin = await Admin.findOne({ where: { email, password } });
+
+        if (!admin) {
+            return res.status(400).send("Invalid credentials.");
+        };
+
+        res.cookie("access_token", generateAccessToken(admin), {
+            httpOnly: true,
+            signed: true
+        });
+
+        res.status(200).send(admin);
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).send("Internal server error.");
     }
 }
 
-module.exports.admin = admin;
+module.exports.login = login;
